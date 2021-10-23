f67e0b5f29d1f0dfa7808c164552cd98f6deedb5
Yusron Hanan <yusron.hanan@medicplus.co.id>
2021-10-23 20:55:19 +0700

    3. Domain Layer Refactoring

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,9 @@
+import 'package:trivia_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  
+  NumberTriviaModel({
+required String text,
+required int 
+  })
+}
